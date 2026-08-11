3f7490c95d71fcb4b5c4a8b93fe7906e90249474
riya-chaudhary27 <kunduriya1p@gmail.com>
2026-08-11 16:03:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://aiassistant-y6xs.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
